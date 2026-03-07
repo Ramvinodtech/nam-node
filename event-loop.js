7dac8bcb5fbf95b14cee9edf471d42390239dac2
Rama Vinod <ramvinod.tech@gmail.com>
2026-03-07 07:08:24 +0530

    event loop phases execution trials

--- event-loop.js
+++ event-loop.js
@@ -2,15 +2,23 @@
 const fs = require('fs');
 setTimeout(() => {
     console.log('timeout')
-},0);
+},1000);
 
 process.nextTick(() => {
     console.log('tick');
     Promise.resolve('Promise').then(console.log)
 });
 
+setTimeout(() => {
+    process.nextTick(() => {
+        console.log('tick from timeout ');
+        Promise.resolve('Promise from timeout').then(console.log)
+    });
+    console.log('timeout 2')
+},1000);
+
 fs.readFile('./file.txt', (err, data) => {
-    console.log('File content is read in asyc');
+    console.log('File content is read in async');
     setImmediate(() => console.log('Immediate'))
     Promise.resolve('Promise from File read').then(console.log);
 });
